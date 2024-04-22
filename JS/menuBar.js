88c3ba28191de619f0f6c01b59c9a559fb0c9196
Eduardo Cordero <ecorderoc@est.utn.ac.cr>
2024-04-21 22:41:45 -0600

    Linking the .js files to .html files and changing some fetch to GraphQL API

--- JS/menuBar.js
+++ JS/menuBar.js
@@ -1,3 +1,4 @@
+
 function navEditPlaylist() {
     location.href = 'http://localhost:5500/editPlaylistPage.html';
 }
@@ -5,9 +6,14 @@ function navEditUsers() {
     location.href = 'http://localhost:5500/editUsersPage.html';
 }
 function backPage() {
+    if(localStorage.getItem("childUser")) {localStorage.removeItem("childUser")}
     location.href = 'http://localhost:5500/usersPage.html';
 }
 function logOut() {
     localStorage.clear();
     location.href = 'http://localhost:5500/login.html';
+}
+
+function navAdmin() {
+    location.href = 'http://localhost:5500/adminPage.html';
 }
--- JS/playVideos.js
+++ JS/playVideos.js
@@ -1,19 +1,29 @@
 
 document.addEventListener('DOMContentLoaded', async () => {
+
     const query = selectQuery();
+
     await fetch(`http://localhost:3000/graphql`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            body:JSON.stringify({})
-        }
+            
+        },
+        body: JSON.stringify({ query })
     })
-        .then(response => response.json(query))
-
+        .then(response => response.json())
+        .then(answer => {
+            if (localStorage.getItem("childUser")) {
+                return answer.data.playlistGetByChild
+            } else {
+                return answer.data.playlistGetByFather
+            }
+        }) 
         .then(data => {
-            if (!data) {
+            if (!data[0]) {
                 document.getElementById('iframe_space').innerHTML = `<h2>Void</h2>
-            <h3>Yet you don't have a playlist to show, go to the button "Edit PlayList" or click <a href="http://localhost:5500/editPlaylistPage.html">here</a> to go</h3>`;
+                <h3>Yet you don't have a playlist to show, go to the button "Edit PlayList" or click <a href="http://localhost:5500/editPlaylistPage.html">here</a> to go</h3>`;
+                loadSelect(undefined)
             } else {
                 document.getElementById('iframe_space').innerHTML = `<h2>${data[0].name}</h2>
                 <iframe id="video_iframe" style="pointer-events: none;"  width="1000" height="500"
@@ -31,7 +41,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         })
 });
 
-function selectQuery(){
+function selectQuery() {
     const query = `query{
         playlistGetByFather(father:"${localStorage.getItem("currentUser")}"){
             name,
@@ -41,8 +51,8 @@ function selectQuery(){
             }
           }
           }`;
-    if(localStorage.getItem("childUser")){
-        query = `query{
+    if (localStorage.getItem("childUser")) {
+        const query = `query{
             playlistGetByChild(child:"${localStorage.getItem("childUser")}"){
                 name,
                 videos{
@@ -51,15 +61,22 @@ function selectQuery(){
                 }
               }
         }`;
-        return 
+        return query;
     }
     return query
 }
 
 function loadSelect(playlist) {
-    playlist.forEach(element => {
-        document.getElementById('playlist_select').innerHTML += `<option value="${element.URL}">${element.name}</option>`;
-    });
+    if(!playlist){
+        document.getElementById('playlist_select').innerHTML +=
+         `<option value="None selected">Void</option>`;
+    }
+    else{
+        playlist.forEach(element => {
+            document.getElementById('playlist_select').innerHTML +=
+             `<option value="${element.URL}">${element.name}</option>`;
+        });
+    }
 };
 
 function loadVideo(selectTag) {
